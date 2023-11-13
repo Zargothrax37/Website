9cdc47419dcd7f555f111cef8b373ca0f72b04f2
Zargothrax37 <jonathan.kleysman@gmail.com>
2023-11-13 10:45:04 +0100

    Added DB for blogposts

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+
 app = Flask(__name__)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+class Blogpost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    content: Mapped[str] = mapped_column(String)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
